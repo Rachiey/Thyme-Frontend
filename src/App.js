7a92038cea23d550b95fbbd0b5f4124ba606bd6a
Rach <90192629+Rachiey@users.noreply.github.com>
2022-11-18 16:20:44 +0000

    added ingredients route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Switch, Route } from 'react-router-dom';
+import * as Pages from './pages';
 
 function App() {
   return (
+    <>
+    <Switch>
     <div className="App">
       <p>Hello world</p>
     </div>
+    
+    <Route exact path='/'>
+            
+            <Pages.Home />
+          
+          </Route>
+
+    <Route path='/ingredients'>
+          <Pages.Ingredients />
+          <Header />
+        </Route>
+    </Switch>
+    </>
   );
 }
 
