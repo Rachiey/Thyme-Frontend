7b208c6538b7c4cf0a4518a56089b4a9083c18b5
Rach <90192629+Rachiey@users.noreply.github.com>
2022-11-18 16:02:40 +0000

    added ingred info

---
+++ src/pages/Ingredients/Ingredients.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Ingredients.css';
+//import { Link } from 'react-router-dom';
+
+
+
+
+
+export const Home = () => {
+
+return (
+    <>
+    <h1 className="ingredientsHeading">Ingredients</h1> 
+    </>
+)
+}
