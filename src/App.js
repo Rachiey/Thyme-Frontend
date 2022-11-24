353716a8f6d2612c195f78bc75544c7898ffce07
unknown <b_lee12@hotmail.co.uk>
2022-11-24 11:50:22 +0000

    Initial login and register pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
@@ -8,6 +10,8 @@ function App() {
     <div className="App">
       <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
 
           <Route path='*' element={<p>nothing to see here</p>} />
       </Routes>
