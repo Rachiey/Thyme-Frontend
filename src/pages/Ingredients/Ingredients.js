5284b54ebf252124162d9232d068384f1c4fa2a1
Rach <90192629+Rachiey@users.noreply.github.com>
2022-11-29 17:07:50 +0000

    added icons and other static buttons for design

--- src/pages/Ingredients/Ingredients.js
+++ src/pages/Ingredients/Ingredients.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Ingredients.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse} from '@fortawesome/free-solid-svg-icons'
+import { faBookOpen} from '@fortawesome/free-solid-svg-icons'
+import { faUser} from '@fortawesome/free-solid-svg-icons'
+// import ReactTooltip from 'react-tooltip';
+// import {useState} from 'react';
 // import './Ingredients/fonts/AlphaFridgeMagnets.ttf';
 
 //import { Link } from 'react-router-dom';
@@ -9,13 +15,37 @@ import './Ingredients.css';
 
 
 export const Ingredients = () => {
+    // const [isHovering, setIsHovering] = useState(false);
+
+    // const handleMouseOver = () => {
+    //   setIsHovering(true);
+    // };
+  
+    // const handleMouseOut = () => {
+    //   setIsHovering(false);
+    // };
+    
+  
 
 return (
     <>
     
     <div className="iphoneContainer"> 
     <div className="iphoneBackground"> 
-    <div className="fridgeTitle"> Username's Fridge </div>
+    <div className="fridgeTitle"> Username's  &nbsp; <span style= {{color: "red"}}> F</span>
+                                            <span style= {{color: "#FDDA0D"}}> r</span>
+                                            <span style= {{color: "blue"}}> i</span>
+                                            <span style= {{color: "#F28C28"}}> d</span>
+                                            <span style= {{color: "#32CD32"}}> g</span>
+                                            <span style= {{color: "#720e9e"}}> e</span>
+                                            </div>
+    <div className="logOutButton">  <span style= {{color: "red"}}> L</span>
+                                            <span style= {{color: "#FDDA0D"}}> o</span>
+                                            <span style= {{color: "blue"}}> g</span>
+                                            &nbsp; 
+                                            <span style= {{color: "#F28C28"}}> O</span>
+                                            <span style= {{color: "#32CD32"}}> u</span>
+                                            <span style= {{color: "#720e9e"}}> t</span></div>
     <div className="shelfOne"></div>
     <div className="shelfTwo"></div>
     <div className="shelfThree"></div>
@@ -23,9 +53,12 @@ return (
         <div className="addButton">+</div>
     </div>
     <div className="bottomMenu">
-        <div className="homeButton"></div>
-        <div className="recipeButton"></div>
-        <div className="profileButton"></div>
+ 
+  <div className="homeButton"><FontAwesomeIcon icon={faHouse} style= {{color:"white"}} /> </div>
+
+        {/* <div className="homeButton" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} > <FontAwesomeIcon icon={faHouse} />   {isHovering && <h2>Home</h2>} </div> */}
+        <div className="recipeButton"> <FontAwesomeIcon icon={faBookOpen} style= {{color:"white"}} /></div>
+        <div className="profileButton"> <FontAwesomeIcon icon={faUser} style= {{color:"white"}}  /> </div>
     </div>
     </div>
     </div>
