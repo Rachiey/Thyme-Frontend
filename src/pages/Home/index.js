3fa21df0d36907f9390648d3c1ae209b0f8fda4d
Rach <90192629+Rachiey@users.noreply.github.com>
2022-11-30 10:34:34 +0000

    update

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+export const Home = () => {
+
+    return (
+        <>
+    <h1 className="homeHeading">Home</h1> 
+    <div className="App">
+      <p>Hello world</p>
+    </div>
+        </>
+    )
+    }
+
+
+export default Home;
