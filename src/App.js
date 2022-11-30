e656380938abf40be5d9f9cbd84ccc44567df283
Rach <90192629+Rachiey@users.noreply.github.com>
2022-11-30 13:45:24 +0000

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import * as Pages from './pages';
 
 function App() {
   return (
-    <>
-    <Routes>
     
+    <div className="App">
+   
+      <Routes>
+        <Route exact path='/' element={<Pages.Home />}></Route>
+        <Route path='/ingredients' element={<Pages.Ingredients/>}></Route>
+        <Route path='/shelf' element={<Pages.Shelf/>}></Route>
+        <Route path='/login' element={<Pages.Login />} />
+        <Route path='/register' element={<Pages.Register />} />
 
-    <Route path="/" element={<Pages.Home />} />
-            
-            
-
-    <Route path="/ingredients" element={<Pages.Ingredients />}>
-          
-          
-        </Route>
-    </Routes>
-    </>
+        <Route path='*' element={<p>nothing to see here</p>} />
+      </Routes>
+    </div>
+    
   );
 }
 
--- src/pages/Ingredients/index.js
+++ src/pages/Ingredients/index.js
@@ -4,28 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse} from '@fortawesome/free-solid-svg-icons'
 import { faBookOpen} from '@fortawesome/free-solid-svg-icons'
 import { faUser} from '@fortawesome/free-solid-svg-icons'
-
-// import ReactTooltip from 'react-tooltip';
-// import {useState} from 'react';
-// import './Ingredients/fonts/AlphaFridgeMagnets.ttf';
-
-//import { Link } from 'react-router-dom';
-
-
+import 'semantic-ui-css/semantic.min.css'
+import { Link } from 'react-router-dom'
 
 
 
 export const Ingredients = () => {
-    // const [isHovering, setIsHovering] = useState(false);
-
-    // const handleMouseOver = () => {
-    //   setIsHovering(true);
-    // };
-  
-    // const handleMouseOut = () => {
-    //   setIsHovering(false);
-    // };
-    
   
 
 return (
@@ -40,13 +24,13 @@ return (
                                             <span style= {{color: "#32CD32"}}> g</span>
                                             <span style= {{color: "#720e9e"}}> e</span>
                                             </div>
-    <div className="logOutButton">  <span style= {{color: "red"}}> L</span>
+    <Link to="/logout"><div className="logOutButton">  <span style= {{color: "red"}}> L</span>
                                             <span style= {{color: "#FDDA0D"}}> o</span>
                                             <span style= {{color: "blue"}}> g</span>
                                             &nbsp; 
                                             <span style= {{color: "#F28C28"}}> O</span>
                                             <span style= {{color: "#32CD32"}}> u</span>
-                                            <span style= {{color: "#720e9e"}}> t</span></div>
+                                            <span style= {{color: "#720e9e"}}> t</span></div> </Link>
     <div className="shelfOne"> <div className="carrot"> <img style ={{objectFit: 'fill'}} src = {require('./images/carrot.png')} alt="carrot" /> </div>
                                 <div className="cheese"> <img style ={{objectFit: 'fill'}} src = {require('./images/cheese.png')} alt="cheese"  /></div> 
                                 <div className="milk"> <img style ={{objectFit: 'fill'}}  src = {require('./images/milk.png')} alt="milk" /></div> </div>
@@ -58,12 +42,10 @@ return (
         <div className="addButton">+</div>
     </div>
     <div className="bottomMenu">
- 
-  <div className="homeButton"><FontAwesomeIcon icon={faHouse} style= {{color:"white"}} /> </div>
-
-        {/* <div className="homeButton" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} > <FontAwesomeIcon icon={faHouse} />   {isHovering && <h2>Home</h2>} </div> */}
-        <div className="recipeButton"> <FontAwesomeIcon icon={faBookOpen} style= {{color:"white"}} /></div>
-        <div className="profileButton"> <FontAwesomeIcon icon={faUser} style= {{color:"white"}}  /> </div>
+    <Link to="/home"> <div className="homeButton"> 
+    < FontAwesomeIcon icon={faHouse} style= {{color:"white"}} />  </div> </Link>
+    <Link to="/recipes"> <div className="recipeButton">  <FontAwesomeIcon icon={faBookOpen} style= {{color:"white"}} /></div> </Link>
+    <Link to="/profile">  <div className="profileButton"> <FontAwesomeIcon icon={faUser} style= {{color:"white"}}  /> </div> </Link>
     </div>
     </div>
     </div>
