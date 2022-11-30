911fb10b1a40bbb6b0eaf35b82c7cb66f769f719
Rach <90192629+Rachiey@users.noreply.github.com>
2022-11-30 13:25:24 +0000

    updating from main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 // import React from 'react';
-
-export { Home } from './Home/Home';
-export { Ingredients } from './Ingredients/Ingredients';
-
+export { default as Home } from './Home';
+export { default as Ingredients } from './Ingredients';
+export { default as Shelf } from './Shelf';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
