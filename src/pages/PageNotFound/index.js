9223ae168d884d257d5f5c1f1ebc2b6501b132ba
Rach <90192629+Rachiey@users.noreply.github.com>
2023-01-16 11:08:36 +0000

    added 404 page details

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageNotFound from '../assets/images/404.png';
+import './404.css';
 class NotFoundPage extends React.Component{
     render(){
-        return <div>
-            <img src={PageNotFound} alt="empty fridge"  />
+        return <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"800px", flexDirection:"column" }}>
+            <img src={PageNotFound} alt="empty fridge" style={{margin:"100px", display:"block"}} />
             <p style={{textAlign:"center"}}>
-                <p>hello</p>
+                <h1>Oops! The page that you're looking for can't be found. This fridge is empty :(</h1>
               <Link to="/">Return Home</Link>
             </p>
           </div>;
