2ea2333f4a54bef88217c501c208f3b3dbd1d9ed
Rach <90192629+Rachiey@users.noreply.github.com>
2023-03-14 15:06:15 +0000

    fixed front of fridge door and took out unused code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -65,10 +65,15 @@ export const Fridge = () => {
    <div className="fridgeMagnets"> <Link to="/ingredients"> <img src={fridgemagnets} alt="fruit fridge magnets" style={{height: "100px"}} /> </Link> </div>
 
   </div>
-<div className="door-back">  <div className="ingredientsTitle"> Ingredients
-{/* <a href="/ingredients">
-    <span className="link"></span></a>   */}
-    <div className ="logOutBox" style= {{marginTop:'20px'}}>
+<div className="door-back">  
+<div className="fridgeTitleBack"> {username}'s  &nbsp; <span style= {{color: "#31BFF3"}}> F</span>
+                                            <span style= {{color: "#A484E9"}}> r</span>
+                                            <span style= {{color: "#F4889A"}}> i</span>
+                                            <span style= {{color: "#FFAF68"}}> d</span>
+                                            <span style= {{color: "#F6E683"}}> g</span>
+                                            <span style= {{color: "#79D45E"}}> e</span>
+                                            </div>
+                                            <div className ="logOutBox">
     <button className="logOutButton" onClick={LoggedOut}> <span style= {{color: "#FFAF68"}}> L</span>
                                             <span style= {{color: "#F6E683"}}> o</span>
                                             <span style= {{color: "#A484E9"}}> g</span>
@@ -77,13 +82,16 @@ export const Fridge = () => {
                                             <span style= {{color: "#79D45E"}}> u</span>
                                             <span style= {{color: "#F4889A"}}> t</span> </button>
     </div>
-    <div className="fridgeShelves"><div className="shelfOne"> <div className="carrot"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/carrot.png')} alt="carrot" /> </div>
+<div className="ingredientsTitle"> Ingredients </div>
+   <div> <a href="/ingredients"><div className="fridgeShelves"><div className="shelfOne"> <div className="carrot"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/carrot.png')} alt="carrot" /> </div>
                                 <div className="cheese"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/cheese.png')} alt="cheese"  /></div> 
                                 <div className="milk"> <img style ={{objectFit: 'fill'}}  src = {require('../Ingredients/images/milk.png')} alt="milk" /></div> </div>
     <div className="shelfTwo"><div className="broccoli"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/broccoli.png')} alt="carrot" /> </div>
         <div className="orangeJuice"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/orange juice.png')} alt="cheese"  /></div> 
         <div className="tomato"> <img style ={{objectFit: 'fill'}}  src = {require('../Ingredients/images/tomato.png')} alt="milk" /></div> </div>
-        <div className="note"> <img className="notePic" src={note} alt="post it note" style={{height: "175px", transform: 'rotate(20deg)'}}/>  
+    </div></a>
+    <div className='bottomFridge'>   
+       <div className="note"> <img className="notePic" src={note} alt="post it note" style={{height: "175px", transform: 'rotate(20deg)'}}/>  
     <div className="recipeEggs"> <Link to="/recipes" style={{ color: 'inherit', textDecoration: 'inherit'}}>Eggs </Link></div>
     <div className="recipeMilk"> <Link to="/recipes" style={{ color: 'inherit', textDecoration: 'inherit'}}>Milk </Link></div>
     <div className="recipeTextButton"> <Link to="/recipes" style={{ color: 'inherit', textDecoration: 'inherit'}}>Recipes </Link></div>
@@ -91,34 +99,10 @@ export const Fridge = () => {
     <div className="profilePic">  <Link to="/profile"><img src={profilepic} alt="polaroid photos" style={{height: "150px"}}/> </Link></div>
     <div className="profilePicButton"> <Link to="/profile" style={{ color: 'inherit', textDecoration: 'inherit'}}>Profile </Link></div></div></div></div>
 </div>
-{/* 
-<div className="iphoneContainer"> 
-    <div className="iphoneIngredientsBackground"> 
-    <div className="fridgeTitle"> _____'s  &nbsp; <span style= {{color: "red"}}> F</span>
-                                            <span style= {{color: "#FDDA0D"}}> r</span>
-                                            <span style= {{color: "blue"}}> i</span>
-                                            <span style= {{color: "#F28C28"}}> d</span>
-                                            <span style= {{color: "#32CD32"}}> g</span>
-                                            <span style= {{color: "#720e9e"}}> e</span>
-                                            </div>
-  
-    <div className="shelfOne"> <div className="carrot"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/carrot.png')} alt="carrot" /> </div>
-                                <div className="cheese"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/cheese.png')} alt="cheese"  /></div> 
-                                <div className="milk"> <img style ={{objectFit: 'fill'}}  src = {require('../Ingredients/images/milk.png')} alt="milk" /></div> </div>
-    <div className="shelfTwo"><div className="broccoli"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/broccoli.png')} alt="carrot" /> </div>
-                                <div className="orangeJuice"> <img style ={{objectFit: 'fill'}} src = {require('../Ingredients/images/orange juice.png')} alt="cheese"  /></div> 
-                                <div className="tomato"> <img style ={{objectFit: 'fill'}}  src = {require('../Ingredients/images/tomato.png')} alt="milk" /></div> </div>
-    <div className="shelfThree"></div>
-    <div className="shelfFour">
-       
-    </div>
-   
-    </div> */}
-    
-    {/* </div> */}
+
         </>
-    )
-    }
+    
+   ) }
 
 
 export default Fridge;
