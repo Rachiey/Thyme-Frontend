0ddfebd8c75c8d5b764d468cf737507635de0047
Rach <90192629+Rachiey@users.noreply.github.com>
2023-06-06 13:50:42 +0100

    made grocery list

--- src/pages/Recipes/index.js
+++ src/pages/Recipes/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
+import Item from "../../components/Item";
 import './recipes.css';
 import 'semantic-ui-css/semantic.min.css'
 import { useNavigate } from 'react-router'
@@ -11,7 +13,11 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 
 
+
+
 export const Recipes = () => {
+
+  
 
     const [user, setUser] = useState({username: "", email: ""});
     const username = localStorage.getItem("username")
@@ -27,6 +33,55 @@ export const Recipes = () => {
     const homeIcon = <FontAwesomeIcon icon={faHouse} />
     const profileIcon = <FontAwesomeIcon icon={faUser} />
     const ingredientsIcon = <FontAwesomeIcon icon={faUtensils} />
+    
+    const arr = () => {
+      let data = localStorage.getItem("data");
+      if (data) return JSON.parse(localStorage.getItem("data"));
+      else return [];
+    };
+
+    const [item, setItem] = useState("");
+    const [edit, setEdit] = useState(false);
+    const [editId, setEditId] = useState();
+    const [list, setList] = useState(arr);
+    const [error, setError] = useState("");
+  
+    const handleSubmit = (e) => {
+      const newItem = {
+        id: uuidv4(),
+        item: item,
+        complete: false,
+      };
+      e.preventDefault();
+    if (item && item.length <= 25 && !edit) {
+      setList([...list, newItem]);
+      setItem("");
+      setError("");
+    } else if (item && item.length <= 25 && edit && editId) {
+      setList(
+        list.map((el) => {
+          if (el.id === editId) {
+            return { ...el, item: item };
+          }
+          return el;
+        })
+      );
+      setItem("");
+      setEditId(null);
+      setEdit(false);
+      setError("");
+    } else if (!item) setError("Item cannot be blank.");
+    else if (item.length > 25) setError("Character limit is 25.");
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(list));
+  }, [list]);
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
 
     
     return (
@@ -49,9 +104,46 @@ export const Recipes = () => {
                                             <span style= {{color: "#79D45E"}}> u</span>
                                             <span style= {{color: "#F4889A"}}> t</span> </button>
     </div>
-<div className="recipesTitle"> Recipes </div>
-   <div> <div className="fridgeShelves"><div className="shelfOne"> </div>
-    <div className="shelfTwo"> </div>
+    <div className="GroceryList">
+      <h1>Grocery List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          value={item}
+          placeholder="Enter the items"
+          onChange={handleChange}
+        />
+        {edit ? (
+          <button className="btn" type="submit">
+            Edit Item
+          </button>
+        ) : (
+          <button className="btn" type="submit">
+            Add Item
+          </button>
+        )}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
+      <div>
+        {list.map((c, id) => (
+          <Item
+            key={id}
+            id={c.id}
+            item={c.item}
+            list={list}
+            setList={setList}
+            complete={c.complete}
+            setItem={setItem}
+            setEdit={setEdit}
+            setEditId={setEditId}
+          />
+        ))}
+      </div>
+    </div>
+    {/* <div className="shelfTwo"> </div> */}
+
+
     <div className="bottomNavBar">  <Tooltip title='Home'>
       <IconButton style={{color: 'white', fontSize: '50px'}}>
       <Link to='/'>{homeIcon} </Link>
@@ -69,8 +161,8 @@ export const Recipes = () => {
     </Tooltip>  </div> 
  
     </div>
-   </div>
-    </div>
+
+   
 
         </>
     
