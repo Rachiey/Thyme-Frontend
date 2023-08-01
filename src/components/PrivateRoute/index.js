849efa6adb52ab1516dbe4313b443604d2fa4dfb
Rach <90192629+Rachiey@users.noreply.github.com>
2023-08-01 12:33:03 +0100

    updated privateroute.js with useffect

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { useNavigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute = ({ element: Element, ...rest }) => {
   // Check if the user is logged in using local storage or any other authentication logic
   const isLoggedIn = !!localStorage.getItem('isLoggedIn');
-//   const navigate = useNavigate();
+  const navigate = useNavigate();
 
+
+useEffect (() => {
   if (!isLoggedIn) {
     // If user is not logged in, redirect to login page
-    return <Navigate to="/login" replace />;
+    navigate('/login', {replace: true});
   }
+}, [isLoggedIn, navigate]);
+
 
   // If user is logged in, render the child routes inside Outlet
   return (
