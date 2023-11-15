b43cbb78d015b8b718e6288a2c594d1c40533550
Rach <90192629+Rachiey@users.noreply.github.com>
2023-11-15 15:57:06 +0000

    update url

--- src/Urls.js
+++ src/Urls.js
@@ -1,5 +1,5 @@
 const origin = window.location.origin
-const api = "http://127.0.0.1:8000/"
-// const api = "https://teathyme-backend.onrender.com/"
+// const api = "http://127.0.0.1:8000/"
+const api = "https://teathyme-backend.onrender.com/"
 
 module.exports = { origin, api }
