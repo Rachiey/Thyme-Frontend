cbc4f57a75b0a6eb97efd1cf3ce1800d7a60b7b7
Rach <90192629+Rachiey@users.noreply.github.com>
2023-11-22 16:20:53 +0000

    added delete user function

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,10 +5,12 @@ import 'semantic-ui-css/semantic.min.css'
 import { useNavigate } from 'react-router'
 import SavedRecipes from '../../components/SavedRecipes';
 import BottomNavbar from '../../components/BottomNavbar/BottomNavbar'; 
+import axios from 'axios';
+import * as urls from '../../Urls';
 
 
 export const Profile = () => {
-
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
   const [username, setUsername] = useState(localStorage.getItem('userName'));
   const navigate = useNavigate();
 
@@ -25,6 +27,38 @@ useEffect(() => {
   setExpiringSoonItems(filteredItems);
 }, [items, filterItemsExpiringSoon]);
 
+const deleteProfile = () => {
+  // API call to delete the user profile
+  const username = localStorage.getItem('userName');
+  const userAuthToken = localStorage.getItem('token');
+  const deleteUrl = `${urls.api}users/${username}/`;
+
+axios
+.delete(deleteUrl, {
+  headers: {
+    Authorization: `Token ${userAuthToken}`
+  }
+})
+.then((response) => {
+  // Successful deletion, remove local storage data and redirect to login
+  localStorage.removeItem('isLoggedIn');
+  localStorage.removeItem('userName');
+  navigate('/login');
+})
+.catch((error) => {
+  // Handle error
+  console.error('Error deleting profile:', error);
+  // You can display an error message or take appropriate action
+});
+};
+
+const handleDeleteConfirmation = () => {
+  setShowDeleteConfirmation(true);
+};
+
+const cancelDelete = () => {
+  setShowDeleteConfirmation(false);
+};
 
 const handleLogout = () => {
   // Reset the local storage session and navigate to the login page
@@ -63,6 +97,21 @@ const fridgeTitle = username ? `${username}'s` : '';
           </button>
 
 <div className="profileTitle"> Profile </div>
+<button className="deleteProfileButton" onClick={handleDeleteConfirmation}>
+        Delete Profile
+      </button>
+        {/* Delete Profile Confirmation Modal */}
+        {showDeleteConfirmation && (
+        <div className="deleteProfileModal">
+          <div className="deleteProfileModalContent">
+            <h3 className='sureText'>Are you sure you want to delete your profile?</h3>
+            <div className="deleteProfileModalActions">
+              <button className="modalAction1" onClick={deleteProfile}>Yes, Delete</button>
+              <button className="modalAction2"  onClick={cancelDelete}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
    <div> <div className="fridgeShelves">
    <h2 className="expiringTitle">Items Expiring Soon (2 days or less)</h2>
   
